33311912bf782acffff485776bc3c636f8d45b7c
Haejin-ineeji <haejin@ineeji.com>
2024-11-20 17:44:43 +0900

    ✨ [GPS-6] feat: 현재 날짜, 시간 표출

--- src/pages/GT1/GT1.tsx
+++ src/pages/GT1/GT1.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Title from "antd/es/typography/Title";
+import styled from "styled-components";
 
 const GT1: React.FC = () => {
-  return <div>GT1</div>;
+  const now = new Date();
+  const Today = now.toLocaleDateString(); // 현재 날짜
+  const Time = now.toLocaleTimeString(); // 현재 시간
+
+  return (
+    <Flex>
+      <Title level={2}>날짜 및 시간</Title>
+      <Title level={3}>{Today}</Title>
+      <Title level={3}>{Time}</Title>
+    </Flex>
+  );
 };
 
 export default GT1;
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
+`;
