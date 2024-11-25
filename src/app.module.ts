381488c343153fa6a917c86eadfdd29fbc158b0d
jaebin-ineeji <jaebin@ineeji.com>
2024-11-25 11:52:50 +0900

    FEAT: [GPS-8]초기 프로젝트 구조 설정 및 모듈 생성
    - PostgreSQL DB 설정 및 도커 컴포즈 추가
    - Seed, Forecast, Prediction 모듈 기본 구조 생성
    - 각 모듈별 Controller, Service, Test 파일 추가
    - @nestjs/mapped-types 의존성 추가
    
    - docker-compose.yml에 PostgreSQL 15 컨테이너 설정
    - .gitignore에 eps-test-data, node_modules 추가
    - 기본 CRUD 작업을 위한 모듈 구조 초기화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeedModule } from './seed/seed.module';
+import { ForecastModule } from './forecast/forecast.module';
+import { PredictionModule } from './prediction/prediction.module';
 
 @Module({
-  imports: [],
+  imports: [SeedModule, ForecastModule, PredictionModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { SeedService } from './seed.service';
+
+@Controller()
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+}
