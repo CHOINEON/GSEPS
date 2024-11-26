e579b2ce573111dd87e6a6590575d2cb295e3e56
jaebin-ineeji <jaebin@ineeji.com>
2024-11-26 18:58:02 +0900

    Revert "Revert "GPS - 9""

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
+import { ForecastModel } from './forecast/entities/forecast.entity';
 import { ForecastModule } from './forecast/forecast.module';
+import { PredictionModel } from './prediction/entities/prediction.entity';
 import { PredictionModule } from './prediction/prediction.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [SeedModule, ForecastModule, PredictionModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [ForecastModel, PredictionModel],
+      synchronize: true,
+    }),
+    SeedModule,
+    ForecastModule,
+    PredictionModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/forecast/forecast.controller.ts
+++ src/forecast/forecast.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { ForecastService } from './forecast.service';
 
 @Controller('forecast')
 export class ForecastController {
   constructor(private readonly forecastService: ForecastService) {}
+
+  // 3일 예보 데이터를 가져오는 함수
+  @Get('3d')
+  getForecast3Days(@Query('time', ParseIntPipe) time: number) {
+    return this.forecastService.getForecast3Days(time);
+  }
 }
